ebda78107355e84522319b5e103da06aecc65e4b
Mich2899 <mich1576@colorado.edu>
2021-11-14 22:11:41 -0700

    Code to extract data from GPS added GUI code in further sprint

---
+++ gps_serial.py
@@ -0,0 +1,36 @@
+ 
+import time
+import serial
+import string
+import pynmea2
+import RPi GPIO as gpio
+ 
+port = "/dev/ttyAMA0" # the serial port to which the pi is connected.
+ 
+#create a serial object
+ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
+ 
+while 1:
+    try:
+        data = ser.readline()
+    except:
+print("loading") 
+#wait for the serial port to churn out data
+ 
+    if data[0:6] == '$GPGGA': # the long and lat data are always contained in the GPGGA string of the NMEA data
+ 
+        msg = pynmea2.parse(data)
+ 
+#parse the latitude and print
+        latval = msg.lat
+concatlat = "lat:" + str(latval)
+        print concatlat
+#add GUI code here
+ 
+#parse the longitude and print
+longval = msg.lon
+concatlong = "long:"+ str(longval)
+print concatlong
+#add gui code here
+    time.sleep(0.5)#wait a little before picking the next data.
+ 
